55e446eed484225b3afcc3ac28f1a659d6ff17f8
KuuLotus <kuulotusy@gmail.com>
2023-04-20 03:39:50 +0900

    [add]mongoose connect

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelpCamp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("MongoDBコネクションOK!!");
+  })
+  .catch((err) => {
+    console.log("MongoDBコネクションエラー!!");
+    console.log(err);
+  });
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "私の庭",
+    description: "気軽に安くキャンプ!!",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
