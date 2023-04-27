5b839fca745e14b192936e97d9a4f231076e9eaa
KuuLotus <kuulotusy@gmail.com>
2023-04-27 10:57:13 +0900

    [add] delete review

--- app.js
+++ app.js
@@ -134,6 +134,16 @@ app.post(
   })
 );
 
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("ページが見つかりませんでした", 404));
 });
