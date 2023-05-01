82c6350a3ce41ff9c3858d8a1cfaaa09580cd168
KuuLotus <kuulotusy@gmail.com>
2023-05-01 17:11:46 +0900

    [add] sessionConfig

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
+const session = require("express-session");
+
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
@@ -34,6 +35,17 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "mysecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+};
+app.use(session(sessionConfig));
 
 app.get("/", (req, res) => {
   res.render("home");
