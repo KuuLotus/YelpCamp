d28b2e2e90ad08bb4114ddbeeeaf44ab0f3922f1
KuuLotus <kuulotusy@gmail.com>
2023-05-08 15:43:47 +0900

    [fix] Review Model path

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const Campground = require("./models/campground");
-const Review = require("./models/campground");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-
 const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
